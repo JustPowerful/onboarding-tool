4c0cf31cec797bded194b630108cbf254cfa53a8
JustPowerful <ahmedaminedoudech@gmail.com>
2024-06-22 02:46:12 +0100

    + add workspace main page routerlink for better navigation

--- frontend/src/views/workspace/WorkspaceTableView.vue
+++ frontend/src/views/workspace/WorkspaceTableView.vue
@@ -4,15 +4,19 @@ import WorkspaceMemberManager from "@/components/workspace/WorkspaceMemberManage
 import TabButton from "@/components/workspace/TabButton.vue";
 import BoardTab from "@/components/workspace/tabs/BoardTab.vue";
 import type { WorkspaceData, WorkspaceTab } from "@/types";
-import { Settings, Table2, User, UserPlus } from "lucide-vue-next";
+import {
+  ChevronRight,
+  Settings,
+  Table2,
+  User,
+  UserPlus,
+} from "lucide-vue-next";
 import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 const route = useRoute();
 
 const workspaceId = Number(route.params.id);
 const tab = ref<WorkspaceTab>("BOARD");
-
-const toggleMemberManager = ref(false);
 
 // data
 const workspace = ref<WorkspaceData | null>(null);
@@ -34,13 +38,23 @@ onMounted(async () => {
   <!-- make the div take the height of (screen - 14 of tailwinds mesurement) -->
   <div class="h-[calc(100vh-3.5rem)] grid grid-rows-[1fr_9fr]">
     <div
-      class="text-2xl font-semibold flex gap-4 items-center justify-between p-6 border-2"
+      class="text-2xl font-medium flex gap-4 items-center justify-between p-6 border-2"
     >
       <div class="flex items-center justify-center gap-2">
         <div class="border-2 border-zinc-300 p-2 rounded-md">
           <Table2 :size="24" />
         </div>
-        <div v-if="workspace">{{ workspace.name }}</div>
+        <div class="flex items-center" v-if="workspace">
+          <div>
+            <RouterLink
+              class="text-zinc-800 block font-semibold hover:text-red-500"
+              to="/workspaces"
+              >Workspaces</RouterLink
+            >
+          </div>
+          <ChevronRight :size="26" />
+          <div class="font-normal">{{ workspace.name }}</div>
+        </div>
       </div>
       <!-- member manager menu -->
       <WorkspaceMemberManager :workspace-id="workspaceId" />
