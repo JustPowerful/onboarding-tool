63befa2d46c03b6d3ad1e59781e2f048ed396c1e
JustPowerful <ahmedaminedoudech@gmail.com>
2024-06-23 02:02:14 +0100

    / remove 6 character restriction in login schema

--- backend/src/modules/auth/auth.schema.ts
+++ backend/src/modules/auth/auth.schema.ts
@@ -46,9 +46,8 @@ const loginUserSchema = z.object({
   email: z.string().email({
     message: "Email is not valid",
   }),
-  password: z
-    .string()
-    .min(6, { message: "Password must be at least 6 characters long" }),
+  password: z.string(),
+  // .min(6, { message: "Password must be at least 6 characters long" }),
 });
 
 export type CreateUserInput = z.infer<typeof createUserSchema>;
