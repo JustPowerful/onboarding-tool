a8ff09dfa220098c7a0b2096d5df663265fb6da8
JustPowerful <ahmedaminedoudech@gmail.com>
2024-06-26 01:17:19 +0100

    + add solution proposal count for the button

--- frontend/src/components/workspace/proposals/ProposalManager.vue
+++ frontend/src/components/workspace/proposals/ProposalManager.vue
@@ -1,19 +1,42 @@
 <script setup lang="ts">
+import { AxiosPrivate } from "@/api";
 import type { TaskData } from "@/types";
-import { ListChecks, X } from "lucide-vue-next";
-import { ref, defineProps } from "vue";
+import { CheckCheck, ListChecks, X } from "lucide-vue-next";
+import { ref, defineProps, onMounted } from "vue";
 const toggle = ref(false);
 const props = defineProps<{
   task: TaskData;
 }>();
+
+const count = ref(0);
+async function getCount() {
+  try {
+    const { data } = await AxiosPrivate.get(
+      `/proposal/paginate?taskId=${props.task.id}&search=&page=0&status=PENDING`
+    );
+    count.value = data.totalProposals;
+  } catch (error) {
+    throw error;
+  }
+}
+
+onMounted(() => {
+  getCount();
+});
 </script>
 <template>
   <button
     @click="toggle = !toggle"
-    class="bg-zinc-300 hover:bg-zinc-400 w-full p-1 rounded-md text-slate-700 flex items-center justify-center gap-1"
+    class="relative bg-zinc-300 hover:bg-zinc-400 w-full p-1 rounded-md text-slate-700 flex items-center justify-center gap-1"
   >
     <CheckCheck :size="18" />
     Proposed Solutions
+    <div
+      v-if="count > 0"
+      class="absolute -top-2 -right-2 bg-red-500 text-white text-sm w-6 h-6 flex items-center justify-center rounded-full"
+    >
+      <span>{{ count }}</span>
+    </div>
   </button>
   <!-- Toggled window background -->
   <div
