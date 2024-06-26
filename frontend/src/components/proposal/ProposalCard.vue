4b265d16d7fb75f74f1f26e79a75bbdd9836d966
JustPowerful <ahmedaminedoudech@gmail.com>
2024-06-26 01:16:09 +0100

    + add proposal accept and reject button for manager

--- frontend/src/components/proposal/ProposalCard.vue
+++ frontend/src/components/proposal/ProposalCard.vue
@@ -2,9 +2,10 @@
 import { AxiosPrivate } from "@/api";
 import type { Proposal, TaskData, UserData } from "@/types";
 import ProposalModal from "@/views/proposals/ProposalModal.vue";
-import { Edit, Trash, X } from "lucide-vue-next";
+import { Check, Edit, Trash, X } from "lucide-vue-next";
 import { ref } from "vue";
 import { defineProps } from "vue";
+
 const props = defineProps<{
   proposal: Proposal;
   user: UserData;
@@ -46,8 +47,63 @@ function parseTextFromMarkDown(htmlString: string) {
 
   return text;
 }
+
+const toggleStatus = ref<"APPROVE" | "DECLINE" | null>(null);
+async function updateStatus(status: Proposal["status"]) {
+  try {
+    await AxiosPrivate.patch(`/proposal/update`, {
+      id: props.proposal.id,
+      name: props.proposal.name,
+      description: props.proposal.description,
+      stack: props.proposal.stack,
+      status,
+    });
+    props.fetchProposals();
+    toggleStatus.value = null;
+  } catch (error) {}
+}
 </script>
 <template>
+  <!-- status modal -->
+  <div
+    v-if="toggleStatus"
+    class="fixed top-0 left-0 w-full h-screen bg-black bg-opacity-50 z-50 flex items-center justify-center"
+  >
+    <div class="w-full max-w-[700px] bg-white p-6 rounded-md">
+      <div class="text-lg font-semibold">Update proposal status</div>
+      <div class="">
+        <span v-if="toggleStatus === 'APPROVE'"
+          >Are you sure you want to approve this solution proposal?</span
+        >
+        <span v-if="toggleStatus === 'DECLINE'"
+          >Are you sure you want to decline this solution proposal?</span
+        >
+      </div>
+      <div class="flex justify-end gap-4">
+        <button
+          v-if="toggleStatus === 'APPROVE'"
+          @click="updateStatus('APPROVED')"
+          class="bg-green-500 text-white p-2 rounded-md"
+        >
+          Confirm
+        </button>
+        <button
+          v-if="toggleStatus === 'DECLINE'"
+          @click="updateStatus('REJECTED')"
+          class="bg-red-500 text-white p-2 rounded-md"
+        >
+          Reject
+        </button>
+        <button
+          @click="toggleStatus = null"
+          class="bg-zinc-500 text-white p-2 rounded-md"
+        >
+          Cancel
+        </button>
+      </div>
+    </div>
+  </div>
+
   <div
     v-if="toggleWideView"
     class="fixed top-0 left-0 w-full h-screen bg-black bg-opacity-50 z-[35] flex items-center justify-center"
@@ -125,30 +181,49 @@ function parseTextFromMarkDown(htmlString: string) {
   <div
     class="relative w-full max-w-[800px] flex flex-col gap-1 bg-white shadow-md shadow-zinc-500 p-6 rounded-md"
   >
-    <div class="absolute top-3 right-3 flex gap-2">
-      <ProposalModal
-        :task="props.task"
-        :proposal="props.proposal"
-        :user="props.user"
-        :editMode="true"
-        :fetchProposals="fetchProposals"
-        class="bg-blue-500 hover:bg-blue-600 text-white p-1 rounded-md cursor-pointer"
+    <div class="absolute top-3 right-3 flex flex-col gap-2">
+      <div class="flex items-center justify-end gap-1">
+        <ProposalModal
+          :task="props.task"
+          :proposal="props.proposal"
+          :user="props.user"
+          :editMode="true"
+          :fetchProposals="fetchProposals"
+          class="bg-blue-500 hover:bg-blue-600 text-white p-1 rounded-md cursor-pointer"
+        >
+          <Edit :size="18" />
+        </ProposalModal>
+        <button
+          @click.stop="toggleDelete = true"
+          v-if="
+            (user.role === 'MANAGER' || proposal.userId === user.id) &&
+            proposal.status === 'PENDING'
+          "
+          class=""
+        >
+          <Trash
+            :size="26"
+            class="bg-red-500 hover:bg-red-600 text-white p-1 rounded-md cursor-pointer"
+          />
+        </button>
+      </div>
+      <div
+        class="flex items-center justify-end gap-1"
+        v-if="user.role === 'MANAGER' && proposal.status === 'PENDING'"
       >
-        <Edit :size="18" />
-      </ProposalModal>
-      <button
-        @click.stop="toggleDelete = true"
-        v-if="
-          (user.role === 'MANAGER' || proposal.userId === user.id) &&
-          proposal.status === 'PENDING'
-        "
-        class=""
-      >
-        <Trash
-          :size="26"
-          class="bg-red-500 hover:bg-red-600 text-white p-1 rounded-md cursor-pointer"
-        />
-      </button>
+        <button
+          @click="toggleStatus = 'APPROVE'"
+          class="flex items-center gap-1 text-sm text-white bg-green-500 p-1 rounded-md"
+        >
+          <Check :size="14" /> Approve
+        </button>
+        <button
+          @click="toggleStatus = 'DECLINE'"
+          class="flex items-center gap-1 text-sm text-white bg-red-500 p-1 rounded-md"
+        >
+          <X :size="14" /> Decline
+        </button>
+      </div>
     </div>
 
     <div>
